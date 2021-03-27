f2ddffe2430d449bf4ab27763619273985be1501
tavispalmer <sonictavis@gmail.com>
2021-03-27 13:14:15 -0400

    commit at 1:14 PM

---
+++ build/CMakeFiles/hoohacks2021.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hoohacks2021.dir/main.cpp.o"
+  "hoohacks2021"
+  "hoohacks2021.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hoohacks2021.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /Users/tavispalmer/projects/hoohacks2021
+# Build directory: /Users/tavispalmer/projects/hoohacks2021/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
