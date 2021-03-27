69d3e65afe9a98de560421b76ce71fb6d9b60334
tavispalmer <sonictavis@gmail.com>
2021-03-27 17:11:28 -0400

    commit on Sat 5:11 PM

---
+++ battleship/Controller.hpp
@@ -0,0 +1,19 @@
+#ifndef BATTLESHIP_CONTROLLER_HPP
+#define BATTLESHIP_CONTROLLER_HPP
+
+struct Controller
+{
+    bool right;
+    bool rightPressed;
+
+    bool up;
+    bool upPressed;
+
+    bool left;
+    bool leftPressed;
+
+    bool down;
+    bool downPressed;
+};
+
+#endif
--- main.cpp
+++ main.cpp
@@ -1,46 +1,114 @@
 #include <iostream>
+#include <exception>
 #include <SFML/System.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
 #include "battleship/Config.hpp"
 #include "battleship/Board.hpp"
+#include "battleship/Controller.hpp"
 #include "battleship/Cursor.hpp"
 #include "battleship/Ship.hpp"
 
+sf::RenderWindow window(sf::VideoMode(TILE_SIZE * 10, TILE_SIZE * 10), "hoohacks2021");
+Board board(sf::Vector2u(10, 10));
+Controller controller;
+Cursor cursor;
+
+enum GameState
+{
+    playerMove
+} gameState;
+
+/// Gets this player's move
+void getPlayerMove(sf::RenderWindow& window)
+{
+    // Move the cursor
+    
+}
+
+
+/// Method to handle window events
+void handleEvents(sf::RenderWindow& window, Controller& controller)
+{
+    controller.rightPressed = false;
+    controller.upPressed = false;
+    controller.leftPressed = false;
+    controller.downPressed = false;
+
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+        if (event.type == sf::Event::Closed)
+            window.close();
+        
+        // Keyboard input handling
+        if (event.type == sf::Event::KeyPressed)
+        {
+            switch (event.key.code)
+            {
+                case sf::Keyboard::Right:
+                    controller.right = true;
+                    controller.rightPressed = true;
+                    break;
+                case sf::Keyboard::Up:
+                    controller.up = true;
+                    controller.downPressed = true;
+                    break;
+                case sf::Keyboard::Left:
+                    controller.left = true;
+                    controller.leftPressed = true;
+                    break;
+                case sf::Keyboard::Down:
+                    controller.down = true;
+                    controller.downPressed = true;
+                    break;
+                default:
+                    break;
+            }
+        }
+        if (event.type == sf::Event::KeyReleased)
+        {
+            switch (event.key.code)
+            {
+                case sf::Keyboard::Right:
+                    controller.right = false;
+                    break;
+                case sf::Keyboard::Up:
+                    controller.up = false;
+                    break;
+                case sf::Keyboard::Left:
+                    controller.left = false;
+                    break;
+                case sf::Keyboard::Down:
+                    controller.down = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+/// Main method
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(TILE_SIZE * 10, TILE_SIZE * 10), "hoohacks2021");
     window.setKeyRepeatEnabled(false);
 
-    Board board(sf::Vector2u(10, 10));
     board.addShip(Ship(sf::Vector2i(4, 4), Ship::East, Ship::Carrier));
-
-    Cursor cursor;
 
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-            if (event.type == sf::Event::KeyPressed)
-            {
-                if (event.key.code == sf::Keyboard::Right)
-                    cursor.move(sf::Vector2i(1, 0));
-                if (event.key.code == sf::Keyboard::Up)
-                    cursor.move(sf::Vector2i(0, -1));
-                if (event.key.code == sf::Keyboard::Left)
-                    cursor.move(sf::Vector2i(-1, 0));
-                if (event.key.code == sf::Keyboard::Down)
-                    cursor.move(sf::Vector2i(0, 1));
-            }
-        }
+        handleEvents(window, controller);
 
         window.clear();
-        window.draw(board);
-        window.draw(cursor);
+
+        switch (gameState)
+        {
+            case playerMove: getPlayerMove(window);
+            default: throw std::exception();
+        }
+
         window.display();
     }
 
